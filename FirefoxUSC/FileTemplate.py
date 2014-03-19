3b52c76f1197070975e3a3a962749e3adee68944
scriptingosx <ab@scriptingosx.com>
2014-03-19 14:44:21 -0700

    Change template character in FileTemplate.py to autopkg style %variable% notation

--- FirefoxUSC/FileTemplate.py
+++ FirefoxUSC/FileTemplate.py
@@ -12,9 +12,18 @@ from autopkglib import Processor, ProcessorError
 
 __all__ = ["FileTemplate"]
 
+class MyTemplate(Template):
+    pattern = r"""
+    %(?:
+      (?P<escaped>%)                     | # Escape sequence of two delimiters
+      (?P<named>[a-zA-Z_][a-zA-Z_0-9]*)  | # delimiter and a Python identifier
+      (?P<braced>[a-zA-Z_][a-zA-Z_0-9]*) | # delimiter and a braced identifier
+      (?P<invalid>)                        # Other ill-formed delimiter exprs
+    )%
+    """
 
 class FileTemplate(Processor):
-    description = "Uses Python's string.Template() class to substitute placeholders in a file with autopkg variables. The placeholders use the default '$identifier' or '${identifier}' syntax."
+    description = "Uses Python's string.Template() class to substitute placeholders in a file with autopkg variables. The placeholders use the autopkg '%identifier%' syntax."
     input_variables = {
         "template_path": {
             "required": True,
@@ -45,13 +54,14 @@ class FileTemplate(Processor):
         template_string = infile.read()
         infile.close()
         
-        substituted_string = Template(template_string).safe_substitute(self.env)
+        substituted_string = MyTemplate(template_string).safe_substitute(self.env)
         
         outfile = open(destination_path, 'w')
         outfile.write(substituted_string)
         outfile.close()
         
-        #self.output("Cleaned version string %s" % self.env["version"])
+        self.output("Templated " + template_path + " to " + destination_path);
+        
 
 if __name__ == '__main__':
     processor = FileTemplate()
